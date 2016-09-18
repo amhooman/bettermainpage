e921d893f7f99f5374df967194e7df652053bdc3
Jib <jibreilhoneine@gmail.com>
2016-09-18 11:42:23 +1000

    Links open in new tab on click, current tab via keyboard shortcut, added bg colour before image loads

--- script.js
+++ script.js
@@ -32,7 +32,7 @@ function getWeather(location) {
         unit: 'c',
         success: function(weather) {
             $('.weather').html('In ' + weather.city + ', ' + weather.region + ', the weather is ' + weather.currently + ', the temperature is ' + weather.temp + '&deg;, and the wind is ' + weather.wind.speed + weather.units.speed + ' <span class="no-transform">' + weather.wind.direction + '</span>');
-            $('.weatherlink').html('<a href="' + weather.link + '">More details (w)</a>');
+            $('.weatherlink').html('<a target="_blank" href="' + weather.link + '">More details (w)</a>');
         },
         error: function(error) {
             $('.weather').html('Sorry, ' + error);
